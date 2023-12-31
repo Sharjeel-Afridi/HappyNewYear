701829d74d215ce6fd3840ac37875855b72e1846
Sharjeel-Afridi <afridisharjeel8@gmail.com>
2023-12-31 14:33:23 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,32 +1,54 @@
-function generateWish() {
-    const socialLink = document.getElementById('socialLink').value;
-    // Extract username or ID from the social media link (you need to implement this logic)
+const input = document.getElementById('input');
+const submitbtn = document.getElementById('submit');
+const imagediv = document.getElementById('image');
+const wishContainer = document.getElementById('wishContainer');
 
-    // Create a new wish page dynamically
-    const wishPage = document.createElement('a');
-    wishPage.href = `wish.html?user=${username}`;
-    wishPage.innerText = `Wish for ${username}`;
 
-    // Append the wish link to the container
-    document.getElementById('wishContainer').appendChild(wishPage);
-}
+
+submitbtn.addEventListener('click', async () => {
+  const username = input.value;
+  wishContainer.classList.add('show');
+  if( username != ""){
+    wishContainer.innerHTML = '';
+    const userData = await getPublicUserData(username);
+    const image = userData.avatar_url;
+    
+    localStorage.setItem('image', image);
+    const wishApiResponce = await fetch("https://api.quotable.io/random?tags=Love").then((resp)=>{return resp.json()}).then(data => {return data});
+    const wishText = wishApiResponce.content;
+    
+    generateWish(image, wishText);
+  }
+});
+
 
 
 async function getPublicUserData(username) {
-    const apiUrl = `https://api.github.com/users/${username}`;
-  
-    try {
-      const response = await fetch(apiUrl);
-      const userData = await response.json();
-  
-      // Extract and use the data as needed
-      console.log(userData);
-    } catch (error) {
-      console.error('Error fetching public user data:', error);
-    }
+  const apiUrl = `https://api.github.com/users/${username}`;
+
+  try {
+    const response = await fetch(apiUrl);
+    const userData = await response.json();
+
+    return userData;
+  } catch (error) {
+    console.error('Error fetching public user data:', error);
   }
+}
+
+function generateWish(image, wishText) {
+  const wishPage = document.createElement('div');
+  wishPage.setAttribute('id', 'wish-div');
   
-  // Example usage
-  const username = 'exampleUser';
-  getPublicUserData(username);
+  wishPage.innerHTML = `<h1>HAPPY NEW YEAR 2024</h1> <img src='${image}'>`;
+  
+  
+  const wishParagraph = document.createElement('p');
+  wishParagraph.textContent = wishText;
+  wishPage.appendChild(wishParagraph);
+
+  wishContainer.appendChild(wishPage);
+}
+
+
   
